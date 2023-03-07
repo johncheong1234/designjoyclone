18f7d2e47fed2d09e4b08f90c92295bc1f1d2d08
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-07 19:48:27 +0800

    right left hero image containers

--- src/features/hero/Hero.js
+++ src/features/hero/Hero.js
@@ -1,10 +1,30 @@
 import React from 'react';
 // import as reactComponent
 import headerImage from '../../assets/headerImage.svg';
+import leftHeroImage from '../../assets/left-hero.svg';
+import rightHeroImage from '../../assets/right-hero.svg';
 
 export function Hero() {
     return (
         <div className='hero-section'>
+            <div style={{
+                position: 'absolute',
+                marginTop: '114px'
+            }}
+
+                className='left-hero-image-container'
+            >
+                <img src={leftHeroImage} alt="left-hero" width={'171px'} height={'342px'} />
+            </div>
+            <div style={{
+                position: 'absolute',
+                right: 0,
+                marginTop: '91px'
+            }}
+                className='right-hero-image-container'
+            >
+                <img src={rightHeroImage} alt="left-hero" width={'171px'} height={'308px'} />
+            </div>
             <div className='hero-section-image-container'>
                 <img src={headerImage} alt="header" className='hero-section-image' />
             </div>
@@ -17,8 +37,9 @@ export function Hero() {
 
                 <div className='see-plans-button'>
                     See plans
-                </div>  
+                </div>
             </div>
+
         </div>
     );
 }
