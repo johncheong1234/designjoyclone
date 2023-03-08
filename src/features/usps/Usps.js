917d6f8b25431ee7ea1c440bd7f45e9d09d943f3
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-08 12:50:31 +0800

    more lotties

--- src/features/usps/Usps.js
+++ src/features/usps/Usps.js
@@ -2,6 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Lottie from "lottie-react";
 import dogFootprints from '../../assets/dogFootprints.json';
+import polarbear from '../../assets/polarbear.json';
+import spinner from '../../assets/spinner.json';
 
 export function Usps() {
     return (
@@ -22,13 +24,13 @@ export function Usps() {
                             <p class="usps-detail-text">Subscribe to a plan & request as many designs as you'd like.</p>
                         </div>
                         <div class="usps-detail col-md">
-                            <Lottie animationData={dogFootprints} loop={true} style={{
+                            <Lottie animationData={polarbear} loop={true} style={{
                                 height: '150px',
                             }} />
                             <p class="usps-detail-text">Receive your design within a few business days on average, Monday to Friday.</p>
                         </div>
                         <div class="usps-detail col-md">
-                            <Lottie animationData={dogFootprints} loop={true} style={{
+                            <Lottie animationData={spinner} loop={true} style={{
                                 height: '150px',
                             }} />
                             <p class="usps-detail-text">We'll revise the designs until you're 100% satisfied.
