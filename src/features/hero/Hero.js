cd1112b1789a221f51fe6a421dc4261413149a6d
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-08 13:30:48 +0800

    scramble text animation

--- src/features/hero/Hero.js
+++ src/features/hero/Hero.js
@@ -6,6 +6,36 @@ import rightHeroImage from '../../assets/right-hero.svg';
 import heartIcon from '../../assets/heartIcon.svg';
 
 export function Hero() {
+
+    function scrambleText(e) {
+        const text = e.target.innerText;
+        const originalText = e.target.getAttribute('data-original-text');
+        const letters = 'abcdefghijklmnopqrstuvwxyz';
+        let iterations = 0;
+        const interval = setInterval(() => {
+            let scrambledText = '';
+            for (let i = 0; i < text.length; i++) {
+                if (text[i] === ' ') {
+                    scrambledText += ' ';
+                    continue;
+                }
+
+                if (i < iterations) {
+                    scrambledText += text[i];
+                    continue;
+                }
+                scrambledText += letters[Math.floor(Math.random() * letters.length)];
+            }
+            e.target.innerText = scrambledText;
+            if (iterations >= text.length) {
+                clearInterval(interval);
+                e.target.innerText = originalText;
+            }
+            iterations += 1 / 3;
+
+        }, 20);
+    }
+
     return (
         <div className='hero-section'>
             <div style={{
@@ -30,7 +60,11 @@ export function Hero() {
                 <img src={headerImage} alt="header" className='hero-section-image' />
             </div>
             <div className='hero-section-text-container'>
-                <div className='hero-section-header'>A design agency with a twist
+                <div className='hero-section-header' onMouseOver={
+                    scrambleText
+                }
+                    data-original-text='A design agency with a twist'
+                >A design agency with a twist
                 </div>
                 <div className='hero-section-subheader'>
                     Design subscriptions to scale your business.
--- src/features/usps/Usps.js
+++ src/features/usps/Usps.js
@@ -15,25 +15,25 @@ export function Usps() {
                 <div className='usps-text'>
                     Design as you know it is out the door. Design as you want it just arrived.
                 </div>
-                <div class="usps-details container">
-                    <div class="row">
-                        <div class="usps-detail col-md">
+                <div className="usps-details container">
+                    <div className="row">
+                        <div className="usps-detail col-md">
                             <Lottie animationData={dogFootprints} loop={true} style={{
                                 height: '150px',
                             }} />
-                            <p class="usps-detail-text">Subscribe to a plan & request as many designs as you'd like.</p>
+                            <p className="usps-detail-text">Subscribe to a plan & request as many designs as you'd like.</p>
                         </div>
-                        <div class="usps-detail col-md">
+                        <div className="usps-detail col-md">
                             <Lottie animationData={polarbear} loop={true} style={{
                                 height: '150px',
                             }} />
-                            <p class="usps-detail-text">Receive your design within a few business days on average, Monday to Friday.</p>
+                            <p className="usps-detail-text">Receive your design within a few business days on average, Monday to Friday.</p>
                         </div>
-                        <div class="usps-detail col-md">
+                        <div className="usps-detail col-md">
                             <Lottie animationData={spinner} loop={true} style={{
                                 height: '150px',
                             }} />
-                            <p class="usps-detail-text">We'll revise the designs until you're 100% satisfied.
+                            <p className="usps-detail-text">We'll revise the designs until you're 100% satisfied.
                             </p>
                         </div>
                     </div>
