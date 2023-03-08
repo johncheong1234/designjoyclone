eea5777f7b77c099486c909dd4aa8e7192214e5f
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-08 10:50:03 +0800

    remove comments

--- src/features/scrollPortfolio/scrollPortfolioSlice.js
+++ src/features/scrollPortfolio/scrollPortfolioSlice.js
@@ -9,14 +9,6 @@ export const scrollPortfolioSlice = createSlice({
     initialState,
     // The `reducers` field lets us define reducers and generate associated actions
     reducers: {
-        // increment: (state) => {
-        //   // Redux Toolkit allows us to write "mutating" logic in reducers. It
-        //   // doesn't actually mutate the state because it uses the Immer library,
-        //   // which detects changes to a "draft state" and produces a brand new
-        //   // immutable state based off those changes
-        //   state.value += 1;
-        // },
-
         setPortfolioScrollLeft: (state, action) => {
             state.scrollPortfolioScrollLeft = action.payload.scrollLeft;
         }
