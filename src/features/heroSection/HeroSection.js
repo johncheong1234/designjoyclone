459994adc1f8787c016d91b73cf1ee20c13de7c0
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-28 19:56:53 +0800

    super charge design

--- src/features/heroSection/HeroSection.js
+++ src/features/heroSection/HeroSection.js
@@ -8,6 +8,10 @@ export function HeroSection() {
             <div className='hero-section-image-container'>
                 <img src={DesignAceLogo} alt="header" className='hero-section-image' width={'275px'} height={'72px'}/>
             </div>
+            <div className='hero-section-supercharge'>
+                <div className='hero-section-supercharge-top-line'>Supercharge your business</div>
+                <div className='hero-section-supercharge-bottom-line'>through design</div>
+            </div>
         </div>
     )
 }
