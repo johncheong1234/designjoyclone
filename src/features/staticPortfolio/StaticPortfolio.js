cf6d0d7c4be3230eaa068bf1d278b31b7131e2f4
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-31 16:27:20 +0800

    adding some space

--- src/features/staticPortfolio/StaticPortfolio.js
+++ src/features/staticPortfolio/StaticPortfolio.js
@@ -3,6 +3,29 @@ import React from 'react';
 export function StaticPortfolio() {
     return (
         <div className='static-portfolio-section'>
+            <div 
+            style = {{
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+            >
+
+            </div>
+            <div
+            style={{
+                width: '10%'
+            }}
+            >
+
+            </div>
+            <div
+            style = {{
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+            >
+
+            </div>
         </div>
     )
 }
