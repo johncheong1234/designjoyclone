d49f1cc3bf1ec18e1b9a20b4a9479489e784fc4f
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-31 15:10:45 +0800

    add button

--- src/features/heroSection/HeroSection.js
+++ src/features/heroSection/HeroSection.js
@@ -6,11 +6,17 @@ export function HeroSection() {
     return (
         <div className='new-hero-section'>
             <div className='hero-section-image-container'>
-                <img src={DesignAceLogo} alt="header" className='hero-section-image' width={'275px'} height={'72px'}/>
+                <img src={DesignAceLogo} alt="header" className='hero-section-image' width={'275px'} height={'72px'} />
             </div>
             <div className='hero-section-supercharge'>
                 <div className='hero-section-supercharge-top-line'>Supercharge your business</div>
                 <div className='hero-section-supercharge-bottom-line'>through design</div>
+                <div className='hero-section-description'>
+                    A design agency of designers with tech, growth and finance experience
+                </div>
+                <div className='white-see-plans-button'>
+                    Book a free consultation
+                </div>
             </div>
         </div>
     )
